77c57467d75629bf32a796832c27b9dde1202a1b
tom_glb <goodMorning_glb@163.com>
2022-04-05 18:41:51 +0800

    first commit

---
+++ src/main/java/com/meicai/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.meicai.git;
+
+/**
+ * @author heng.zhang
+ * @version 1.0.0
+ * @ClassName GitTest.java
+ * @createTime 2022年04月05日 18:36:00
+ * @Description TODO
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+    }
+}
